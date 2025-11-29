a2297ec102f53c06259ba7d4b76109097fb8513c
ame10905 <mirroring419@icloud.com>
2025-11-30 00:50:26 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -130,6 +130,11 @@ const nextConfig = {
       'p1.qhimg.com',
       'webmention.io',
       'ko-fi.com'
+      
+    // 🔥 Notion 新 S3 域名（必须添加）
+    'prod-files-secure.s3.us-west-2.amazonaws.com',
+    's3.us-west-2.amazonaws.com',
+    'prod-files-secure.s3.amazonaws.com'
     ],
     // 图片加载器优化
     loader: 'default',
